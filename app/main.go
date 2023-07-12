74b64065a684fbcf94479ff9144f6d70986feaa4
Simon Zuluaga <simozuluaga@gmail.com>
2023-07-12 11:50:47 -0500

    feature: Add main file to bootstrap the server

---
+++ app/main.go
@@ -0,0 +1,66 @@
+package main
+
+import (
+	"database/sql"
+	"fmt"
+	"log"
+	"net/url"
+	"time"
+
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
+	_ "github.com/lib/pq"
+	_userHttpDelivery "github.com/sicozz/papyrus/user/delivery/http"
+	_userRepo "github.com/sicozz/papyrus/user/repository/postgres"
+	_userUsecase "github.com/sicozz/papyrus/user/usecase"
+	"github.com/spf13/viper"
+)
+
+func init() {
+	viper.SetConfigFile(`config.json`)
+	err := viper.ReadInConfig()
+	if err != nil {
+		panic(err)
+	}
+
+	if viper.GetBool(`debug`) {
+		log.Println("Service RUN on DEBUG mode")
+	}
+}
+
+func main() {
+	dbHost := viper.GetString(`database.host`)
+	dbPort := viper.GetString(`database.port`)
+	dbUser := viper.GetString(`database.user`)
+	dbPass := viper.GetString(`database.pass`)
+	dbName := viper.GetString(`database.name`)
+	connection := fmt.Sprintf("postgres://%s:%s@%s:%s/%s", dbUser, dbPass, dbHost, dbPort, dbName)
+	val := url.Values{}
+	val.Add("sslmode", "disable")
+	dsn := fmt.Sprintf("%s?%s", connection, val.Encode())
+	dbConn, err := sql.Open(`postgres`, dsn)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+	err = dbConn.Ping()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer func() {
+		err := dbConn.Close()
+		if err != nil {
+			log.Fatal(err)
+		}
+	}()
+
+	e := echo.New()
+	e.Use(middleware.CORS())
+
+	timeoutContext := time.Duration(viper.GetInt("context.timeout")) * time.Second
+	ur := _userRepo.NewPostgresUserRepository(dbConn)
+	uu := _userUsecase.NewUserUsecase(ur, timeoutContext)
+	_userHttpDelivery.NewUserHandler(e, uu)
+	e.Logger.Fatal(e.Start(":9090"))
+}

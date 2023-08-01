4cb6cbf0cd25002ac8d7ec20142849259838c920
Simon Zuluaga <simozuluaga@gmail.com>
2023-08-01 15:33:45 -0500

    chore: Create migration for base superadmin

---
+++ misc/migrations/20230731191120_insert_superadmin.up.sql
@@ -0,0 +1,10 @@
+INSERT INTO user_ (username, email, password, name, lastname, role, state)
+VALUES (
+    'pps_admin',
+	'pps_admin@pps_admin.com',
+	'pps_admin',
+	'admin',
+	'admin',
+	3,
+	1
+);

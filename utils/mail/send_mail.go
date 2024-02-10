ea7b1eff70172e27e1f5449d3c0d2277f7a8eff6
Simon Zuluaga <simozuluaga@gmail.com>
2024-02-10 09:21:36 -0500

    feature: add send mail functionality

---
+++ utils/mail/send_mail.go
@@ -0,0 +1,29 @@
+package mail
+
+import (
+	"fmt"
+	"net/smtp"
+)
+
+const (
+	Server  = "smtp.masterplac.com"
+	Sender  = "info@masterplac.com"
+	Subject = "Subject: Mensaje de PAPYRUS\r\n\r\n"
+	Passwd  = "Mail.2012+"
+	Port    = 465
+)
+
+func sendMail(to, msg string) error {
+	auth := smtp.PlainAuth("", Sender, Passwd, Server)
+
+	sliceTo := []string{to}
+	byteMsg := []byte(fmt.Sprintf("%v%v", Subject, msg))
+
+	return smtp.SendMail(
+		fmt.Sprintf("%v:%v", Server, 465),
+		auth,
+		Sender,
+		sliceTo,
+		byteMsg,
+	)
+}
